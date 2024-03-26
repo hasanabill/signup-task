63c3930be5a8e57809931537b2fe778552e10fe0
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-03-26 21:55:05 +0600

    errors fixed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,11 +7,13 @@ import { FcGoogle } from "react-icons/fc";
 import { BsMicrosoft } from "react-icons/bs";
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { MdError } from "react-icons/md";
 
 
 const SignUp = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [language, setLanguage] = useState('English');
     const [form, setForm] = useState(false);
     const {
         register,
@@ -31,7 +33,7 @@ const SignUp = () => {
                 <p className='absolute bottom-[24px] mb-[20px] text-[14px]'>© Typeform</p>
             </div>
 
-            <div className="flex flex-col justify-between bg-[#ffffff] lg:w-[55.4%] w-full h-screen absolute right-0 lg:rounded-s-2xl">
+            <div className="flex flex-col justify-between bg-[#ffffff] lg:w-[55.4%] w-full h-screen absolute right-0 lg:rounded-s-2xl overflow-x-auto">
                 <div className='flex justify-between px-[24px] py-[8px] h-[52px]'>
                     <div className='flex items-center gap-1 text-[14px]' onClick={() => setIsOpen(!isOpen)}>
                         <FaGlobeAmericas />
@@ -39,10 +41,10 @@ const SignUp = () => {
                             <option value="english"><p>English</p></option>
                             <option value="espanol"><p>Espanol</p></option>
                         </select> */}
-                        <p className='flex cursor-pointer items-center select-none relative'>English <RiArrowDownSLine /></p>
+                        <p className='flex cursor-pointer items-center select-none relative'>{language}<RiArrowDownSLine /></p>
                         <div className={`absolute w-[100px] shadow-lg top-10 left-12 ${isOpen ? 'inline-block' : 'hidden'}`}>
-                            <p className='cursor-pointer px-3 py-1 hover:bg-slate-200 select-none'>English</p>
-                            <p className='cursor-pointer px-3 py-1 hover:bg-slate-200 select-none'>Espanol</p>
+                            <p onClick={() => setLanguage("English")} className='cursor-pointer px-3 py-1 hover:bg-slate-200 select-none'>English</p>
+                            <p onClick={() => setLanguage("Espanol")} className='cursor-pointer px-3 py-1 hover:bg-slate-200 select-none'>Espanol</p>
                         </div>
 
                     </div>
@@ -63,11 +65,11 @@ const SignUp = () => {
                     <h2 className='text-[24px] text-[#5e5e5e] text-center px-16 md:px-0  mb-[24px]'>Get better data with conversational forms, surveys, quizzes & more.</h2>
                     {
                         form ? <>
-                            <form className='flex flex-col justify-center items-center gap-2' onSubmit={handleSubmit(onSubmit)}>
+                            <form className='flex flex-col justify-center items-start gap-2' onSubmit={handleSubmit(onSubmit)}>
                                 <input placeholder='Email' className='w-[242px] h-[38px] px-[8px] py-[6px] rounded-md border border-gray-400' type='email' {...register("email", { required: true })} />
-                                {errors.email && <span className='text-red-500 text-[14px]'>Email is required</span>}
+                                {errors.email && <span className='text-red-700 text-[14px] flex items-center gap-2'><MdError /> This field cannot be left blank</span>}
                                 <input placeholder='Password' className='w-[242px] h-[38px] px-[8px] py-[6px] rounded-md border border-gray-400' type='password' {...register("password", { required: true })} />
-                                {errors.password && <span className='text-red-500 text-[14px]'>Password is required</span>}
+                                {errors.password && <span className='text-red-700 text-[14px] flex items-center gap-2'><MdError />This field cannot be left blank</span>}
                                 <label className='flex w-[242px] justify-center items-start  gap-1'>
                                     <input type="checkbox" className="checkbox inline" {...register("terms", { required: true })} />
                                     <p className='w-[252px] text-[14px]'>I agree to Typeform’s
@@ -76,7 +78,7 @@ const SignUp = () => {
                                         <a className='underline' href="#">Privacy Policy</a>
                                         and <a className='underline' href="#">Data Processing Agreement</a>.</p>
                                 </label>
-                                {errors.terms && <span className='text-red-500 w-[252px] text-[14px]'>Please accept the terms and conditions to finish the signup</span>}
+                                {errors.terms && <span className='text-red-700 w-[252px] text-[14px] flex gap-2'> <MdError className='text-[23px]' />Please accept the terms and conditions to finish the signup</span>}
                                 <div className="collapse w-[242px] text-[14px] collapse-arrow">
                                     <input type="checkbox" />
                                     <div className="collapse-title">
@@ -85,24 +87,30 @@ const SignUp = () => {
                                     <div className="collapse-content">
                                         <div className='mb-2'>
                                             <p>Get useful tips, inspiration, and offers via e-communication.</p>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label >Yes</label>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label>No</label>
+                                            <div className='flex gap-1 items-center'>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label >Yes</label>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label>No</label>
+                                            </div>
                                         </div>
                                         <div className='mb-2'>
                                             <p>Tailor Typeform to my needs based on my activity.See Privacy Policy</p>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label >Yes</label>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label>No</label>
+                                            <div className='flex gap-1 items-center'>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label >Yes</label>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label>No</label>
+                                            </div>
                                         </div>
                                         <div className='mb-2'>
                                             <p>Enrich my data with select third parties for more relevant content.See Privacy Policy</p>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label >Yes</label>
-                                            <input className='mx-2' type="radio" {...register("newsletter")} />
-                                            <label>No</label>
+                                            <div className='flex gap-1 items-center'>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label >Yes</label>
+                                                <input className='mx-2 radio' type="radio" {...register("newsletter")} />
+                                                <label>No</label>
+                                            </div>
                                         </div>
                                         <p className='text-gray-500'>You can update your preferences in your Profile at any time</p>
                                     </div>
